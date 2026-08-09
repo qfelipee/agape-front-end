5e466db3e47abe31a36c88adef3c05f1f5162d98
felipzp <felipe.ferraz@sptech.school>
2026-08-09 14:21:32 -0300

    configurando url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export async function login(email, senha) {
   const response = await fetch(`${API_URL}/auth/login`, {
